62c7b058773dcb1aabd8bbf7316fd91d01638414
Ben Hockley <hockleyben4@gmail.com>
2026-02-10 19:52:57 +0000

    Fixed image build in actions

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -38,7 +38,7 @@ export const projects: Project[] = [
       "Website developed for client at Legal and General. Enables Employees to send messages, make and play quizzes, and report on their mood.",
     tags: ["Flask", "Bootstrap", "JavaScript", "SQLite"],
     link: "https://github.com/ben-hockley/Client-Project-Y1",
-    image: "/L&G1.png",
+    image: "https://blacklioninsurance.co.uk/wp-content/uploads/2023/11/legal-and-general-2023.jpg",
     category: "academic",
   },
   {
@@ -54,7 +54,7 @@ export const projects: Project[] = [
       "JUnit tests",
     ],
     link: "https://github.com/ben-hockley/Client-Project-Y2",
-    image: "/NHS2.png",
+    image: "https://emedia1.nhs.wales/HEIW2/cache/file/2BA1FC30-611C-4CC6-A367B65BCEC88E0B.png",
     category: "academic",
   },
   {
@@ -63,7 +63,7 @@ export const projects: Project[] = [
       "Hack the diff winning project developed based on problem proposal from Unifying Solutions. Enables colleagues to celebrate each other's success, and amass points to redeem prizes.",
     tags: ["Next.js", "React", "Tailwind CSS", "Supabase"],
     link: "https://github.com/kaalxvairab/shoutout",
-    image: "/shoutout.png",
+    image: "https://private-user-images.githubusercontent.com/151195798/546618375-cac94d22-631b-4cc1-bb2d-9c998998b6d4.png?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NzA3NTMzNDgsIm5iZiI6MTc3MDc1MzA0OCwicGF0aCI6Ii8xNTExOTU3OTgvNTQ2NjE4Mzc1LWNhYzk0ZDIyLTYzMWItNGNjMS1iYjJkLTljOTk4OTk4YjZkNC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjYwMjEwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI2MDIxMFQxOTUwNDhaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1kMGRjYTc3MWJiYzkyOGYyNzA4NzUyZmU3NjcyZGI0YmE1NWY4NDdjNWU3YWZmYWRjZjA5Yjc2MWM5YjdjMGY4JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.8EgfthAm7hV4t3Ha66K1vwQ8WbUv6A1wx_zzDvKY-10",
     category: "hackathon",
   },
 ];
